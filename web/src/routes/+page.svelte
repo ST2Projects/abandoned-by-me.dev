a69c7591ca775b3888f45942d13ec35950f089cc
Ben <ben@st2projects.com>
2024-07-17 21:34:24 +0100

    Remove var import

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import {supabase} from "$lib/supabaseClient.js";
 </script>
 
 <svelte:head>
